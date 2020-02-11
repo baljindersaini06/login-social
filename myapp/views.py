5e22d903ec508acaf58d333d2eb86129faeefe7c
nishtha95 <nishtha0995@gmail.com>
2020-02-11 17:56:22 +0530

    changes

--- myapp/views.py
+++ myapp/views.py
@@ -1392,21 +1392,12 @@ def document_detail(request,doc_id):
     employee_count=Employee.objects.filter(company_name=compdetail.id).count()
 
 
-    doc_detail=Documents.objects.get(id=doc_id)
-    files=Document_File.objects.filter(document=doc_detail.id)
-    files_exists=Document_File.objects.filter(document=doc_detail.id).exists()
-    
-    links=Document_Links.objects.filter(document=doc_detail.id)
-    links_exists=Document_Links.objects.filter(document=doc_detail.id).exists()
     url_text=request.build_absolute_uri()
     print(url_text)
     gmail_share_string=quote(url_text)
     print(gmail_share_string)
     share_string=quote(doc_detail.title)
-
-
-
-    return render(request,'myapp/document_detail.html',{'doc_detail':doc_detail,'files':files,'files_exists':files_exists,'links':links,'links_exists':links_exists,'compdetail':compdetail,'loc_count':loc_count,'web_count':web_count,'lic_count':lic_count,'doc_count':doc_count,'packs_count':packs_count,'device_count':device_count,'employee_count':employee_count,'doc_detail':doc_detail,'gmail_share_string':gmail_share_string,'files':files,'share_string':share_string,'files_exists':files_exists,'links':links,'links_exists':links_exists})
+    return render(request,'myapp/document_detail.html',{'doc_detail':doc_detail,'files':files,'files_exists':files_exists,'links':links,'links_exists':links_exists,'compdetail':compdetail,'loc_count':loc_count,'web_count':web_count,'lic_count':lic_count,'doc_count':doc_count,'packs_count':packs_count,'device_count':device_count,'employee_count':employee_count,'gmail_share_string':gmail_share_string,'share_string':share_string})
 
 
 
